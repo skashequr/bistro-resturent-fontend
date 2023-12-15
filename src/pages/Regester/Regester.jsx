3a6928cc8196b3fe86343f6c0fa201fb3b48c301
skashequr <learningwithextracare@gmail.com>
2023-12-15 16:35:32 +0600

    solve the issu

--- src/pages/Regester/Regester.jsx
+++ src/pages/Regester/Regester.jsx
@@ -7,9 +7,13 @@ import {
 import { AuthContex } from "../../AuthProvider/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+
 const Regester = () => {
+  const axiosPublic = useAxiosPublic() // Heare i use some issu . I face tjhe problem . 
   const navigate = useNavigate();
-  const {createUserWithEmailAndpass} = useContext(AuthContex)
+  const { createUserWithEmailAndpass } = useContext(AuthContex);
   // console.log(loginWithEmail);
   const [disabled, setDisabled] = useState(true);
   useEffect(() => {
@@ -19,35 +23,57 @@ const Regester = () => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    createUserWithEmailAndpass(email,password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      navigate('/')
-      console.log(user);
-      // ...
-    })
-    .catch((error) => {
-    //   const errorCode = error.code;
-    //   const errorMessage = error.message;
-    console.log(error);
-    });
+    createUserWithEmailAndpass(email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        const userInfo = {
+          name: e.target.name.value, 
+          email: e.target.email.value,
+        };
+        
+        axiosPublic.post("/users", userInfo)
+        .then((res) => {
+          if (res.data.insertedId) {
+            console.log("user added to the database");
+
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "User created successfully.",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            navigate("/");
+          }
+        });
+        navigate("/");
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        //   const errorCode = error.code;
+        //   const errorMessage = error.message;
+        console.log(error);
+      });
     console.log(email, password);
   };
- 
+
   const handleValodtedCaptcha = (e) => {
     const user_captcha_value = e.target.value;
     console.log(user_captcha_value);
     if (validateCaptcha(user_captcha_value)) {
       setDisabled(false);
-     
     } else {
       setDisabled(true);
     }
   };
   return (
     <div className="bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto sm:overflow-hidden mt-14">
-                    <h3 className="text-center text-5xl font-bold text-orange-500"> Regester </h3>
+      <h3 className="text-center text-5xl font-bold text-orange-500">
+        {" "}
+        Regester{" "}
+      </h3>
       <div className="px-4 py-8 sm:px-10">
         <div className="relative mt-6">
           <div className="absolute inset-0 flex items-center">
@@ -58,8 +84,18 @@ const Regester = () => {
           </div>
         </div>
         <div className="mt-6">
-          <form onSubmit={handleLogin }>
+          <form onSubmit={handleLogin}>
             <div className="w-full space-y-6">
+              <div className="w-full">
+                <div className=" relative">
+                  <input
+                    type="text"
+                    name="name"
+                    className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    placeholder="Your email"
+                  />
+                </div>
+              </div>
               <div className="w-full">
                 <div className=" relative">
                   <input
@@ -83,8 +119,9 @@ const Regester = () => {
               </div>
               <div className="w-full">
                 <div className=" relative">
-                  
-                  <label className="lable"><LoadCanvasTemplate /></label>
+                  <label className="lable">
+                    <LoadCanvasTemplate />
+                  </label>
                   <input
                     onBlur={handleValodtedCaptcha}
                     // ref={captchaRef}
@@ -98,10 +135,15 @@ const Regester = () => {
               </div>
               <div>
                 <span className="block w-full rounded-md shadow-sm">
-                <div className="form-control mt-6">
-                                {/* TODO: apply disabled for re captcha */}
-                                <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
-                            </div>
+                  <div className="form-control mt-6">
+                    {/* TODO: apply disabled for re captcha */}
+                    <input
+                      disabled={disabled}
+                      className="btn btn-primary"
+                      type="submit"
+                      value="Login"
+                    />
+                  </div>
                 </span>
               </div>
             </div>
