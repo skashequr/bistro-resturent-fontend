19d8afc652089ef6630aa117f1e0bb39302a3ed2
skashequr <learningwithextracare@gmail.com>
2023-12-15 15:07:19 +0600

    add privet route

--- src/Routes/PrivetRoutes.jsx
+++ src/Routes/PrivetRoutes.jsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router";
+import { AuthContex } from "../AuthProvider/AuthProvider";
 
 
-const PrivetRoutes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContex);
+    const location = useLocation();
+
+    if(loading){
+        return <progress className="progress w-56"></progress>
+    }
+
+    if (user) {
+        return children;
+    }
+    return <Navigate to="/login" state={{from: location}} replace></Navigate>
 };
 
-export default PrivetRoutes;
+export default PrivateRoute;
